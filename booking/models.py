b4edd3b0d0c861fb03bf348db35549fc779992ff
Veronica Lourens <Veronica@lourens.cloud>
2022-06-01 18:39:54 +0000

    Create booking model & admin account.

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Booking(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    lesson_name = models.CharField(max_length=30, blank=True)
+    date = models.DateField(blank=False)
+    start_time = models.TimeField(auto_now_add=False)
+    end_time = models.TimeField(auto_now_add=False)
+
+    def __str__(self):
+        return f'{self.user} on {self.date} from {self.start_time} to {self.end_time}'
+
+
+
