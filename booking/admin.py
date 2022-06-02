e4405008a93e64ba4cb8a516151ad9ec2806063b
Veronica Lourens <Veronica@lourens.cloud>
2022-06-02 07:52:24 +0000

    Install summernote & update admin site.

--- booking/admin.py
+++ booking/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 from .models import Booking, Lesson
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Booking)
+
+@admin.register(Booking)
+class BookingAdmin(SummernoteModelAdmin):
+
+    list_display = ('user', 'date', 'start_time', 'lesson_name')
+    list_filter = ('user', 'date')
+    search_fields = ['lesson_name', 'user']
+
+
+
+    # def confirmed_booking(self):
+    #     return confirmed
+
+
+
+
+
+
+#admin.site.register(Booking)
 
 admin.site.register(Lesson)
