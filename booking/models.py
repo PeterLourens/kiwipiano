b96d457799f4c04e9af473f27b768a212968ef2b
Veronica Lourens <Veronica@lourens.cloud>
2022-06-02 07:06:35 +0000

    Update booking models and migrate the changes.

--- booking/models.py
+++ booking/models.py
@@ -6,15 +6,18 @@ from django.utils import timezone
 class Booking(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # email = models.EmailField()
+    #email = models.EmailField()
     lesson_name = models.CharField(max_length=30, blank=True)
     date = models.DateField(blank=False)
+    #time = models.CharField(max_length=30)
     start_time = models.TimeField(auto_now_add=False)
-    end_time = models.TimeField(auto_now_add=False)
-    booked_date = models.DateTimeField(default=timezone.now)
+    #end_time = models.TimeField(auto_now_add=False)
+    #booked_date = models.DateTimeField(default=timezone.now)
+    booked_date = models.DateTimeField(auto_now_add=True)
     confirmed = models.BooleanField(default=True)
    
 
     def __str__(self):
-        return f'{self.user} on {self.date} from {self.start_time} to {self.end_time}'
+        return f'{self.user} on {self.date} at {self.time}'
+
 
