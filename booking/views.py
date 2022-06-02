e536a96eb66b87372c473ecc6fe9d504c933b98c
Veronica Lourens <Veronica@lourens.cloud>
2022-06-02 18:06:38 +0000

    Create lesson view and wireup urls.

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Lesson
 
-# Create your views here.
+
+
+class LessonView(generic.ListView):
+    model = Lesson
+    queryset = Lesson.objects.filter(status=1).order_by('-start_time')
+    template_name = 'index.html'
+    paginate_by = 2
+
