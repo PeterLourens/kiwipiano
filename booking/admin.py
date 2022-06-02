af7021ba23a916a82921494ac19c3bb705f2016b
Veronica Lourens <Veronica@lourens.cloud>
2022-06-02 07:38:10 +0000

    Create lesson model & migrate changes.

--- booking/admin.py
+++ booking/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Booking
+from .models import Booking, Lesson
 
 admin.site.register(Booking)
+
+admin.site.register(Lesson)
--- booking/models.py
+++ booking/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+import datetime
 
 
 class Booking(models.Model):
+
+    """
+    All the lessons can only be booked once on a specific day at a specific time.
+    """
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     #email = models.EmailField()
@@ -18,6 +23,22 @@ class Booking(models.Model):
    
 
     def __str__(self):
-        return f'{self.user} on {self.date} at {self.time}'
+        return f'{self.user} on {self.date} at {self.start_time}'
+
+
+STATUS = [(0, 'Available'), (1, 'Unavailable')]
+class Lesson(models.Model):
+    """
+    The sigle lesson is to be booked by one user.
+    """
+
+    lesson_name = models.CharField(max_length=30, blank=True)
+    #time = models.TimeField(auto_now_add=False)
+    #date = models.DateField(blank=False)
+
+    lesson_status = models.IntegerField(choices=STATUS, default=0)
+
+    def __str__(self):
+        return f'{self.lesson_status}'
 
 
