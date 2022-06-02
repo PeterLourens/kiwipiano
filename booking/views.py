f5480de8b70ecdd4ab3ceb67a12226c9810b6f1e
Veronica Lourens <Veronica@lourens.cloud>
2022-06-02 18:21:37 +0000

    Update urls to test the home view.

--- booking/views.py
+++ booking/views.py
@@ -4,9 +4,18 @@ from .models import Lesson
 
 
 
-class LessonView(generic.ListView):
-    model = Lesson
-    queryset = Lesson.objects.filter(status=1).order_by('-start_time')
-    template_name = 'index.html'
-    paginate_by = 2
+# class LessonView(generic.ListView):
+#     model = Lesson
+#     queryset = Lesson.objects.filter(status=1).order_by('-start_time')
+#     template_name = 'index.html'
+#     paginate_by = 2
+
+
+
+def home(request):
+
+    context = {
+        'lessons': Lesson.objects.all()
+    }
+    return render(request, 'index.html', context)
 
--- kiwipiano/settings.py
+++ kiwipiano/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'kiwipiano.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
