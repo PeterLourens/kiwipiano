21b1f86d38213ce2913f2183080829224d4b8924
Veronica Lourens <Veronica@lourens.cloud>
2022-06-03 09:10:57 +0000

    Comment out the whitenoise code.

--- kiwipiano/settings.py
+++ kiwipiano/settings.py
@@ -144,9 +144,9 @@ STATIC_URL = '/static/'
 
 
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
