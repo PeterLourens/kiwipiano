e377ef16d6e093d6710f58f8093b3a7dcb4955dc
Veronica Lourens <Veronica@lourens.cloud>
2022-06-11 17:21:01 +0000

    Alter fileds in models & migrate changes.

--- booking/models.py
+++ booking/models.py
@@ -3,6 +3,33 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 import datetime
 
+
+
+STATUS = ((0, "Available"), (1, "Unavailable"))
+class Lesson(models.Model):
+    """
+    There are 4 different lessons.
+    """
+
+    LessonType = (
+        ('Beginner', 'Beginner'), 
+        ('Intermediate', 'Intermediate'), 
+        ('Advanced', 'Advanced'), 
+        ('Exams-training', 'Exams-training')
+    )
+    
+ 
+    lesson_name = models.CharField(max_length=30, choices=LessonType)
+    lesson_date = models.DateField(blank=False)
+    start_time = models.TimeField(auto_now_add=False)
+    lesson_status = models.IntegerField(choices=STATUS, default=0)
+
+
+    def __str__(self):
+        return f'Lesson {self.lesson_name} is {self.lesson_status} on {self.lesson_date} at {self.start_time}'
+
+
+
 class Booking(models.Model):
 
     """
@@ -10,7 +37,7 @@ class Booking(models.Model):
     """
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    lesson_name = models.CharField(max_length=30, blank=True)
+    lesson_name = models.ForeignKey(Lesson, on_delete=models.CASCADE)
     date = models.DateField()
     start_time = models.TimeField(auto_now_add=False)
     end_time = models.TimeField(auto_now_add=False)
@@ -24,27 +51,6 @@ class Booking(models.Model):
 
     def __str__(self):
         return f'{self.user} on {self.date} at {self.start_time}'
-
-
-
-
-STATUS = ((0, "Available"), (1, "Unavailable"))
-class Lesson(models.Model):
-    """
-    There are 4 different lessons.
-    """
-
-    LessonType = (('Beginner', 'Beginner'), ('Intermedia', 'Intermedium'), ('Advanced', 'Advanced'), ('Examination-training', 'Examination-training'))
-    
- 
-    lesson_name = models.CharField(max_length=30, choices=LessonType)
-    lesson_date = models.DateField(blank=False)
-    start_time = models.TimeField(auto_now_add=False)
-    lesson_status = models.IntegerField(choices=STATUS, default=0)
-
-
-    def __str__(self):
-        return f'Lesson {self.lesson_name} is {self.lesson_status} on {self.lesson_date} at {self.start_time}'
 
 
 
