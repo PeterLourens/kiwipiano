0f027bbd5cc0457e5ec1d7913ea45d13ef3fb364
Veronica Lourens <Veronica@lourens.cloud>
2022-06-27 20:13:47 +0000

    Add code to auto test urls in test.py

--- booking/tests.py
+++ booking/tests.py
@@ -1,3 +1,78 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from booking.views import home, sign_up, login
+from booking.views import logout_view, admin_login, feedback, profile
+from booking.views import booking_form, BookingDetailView
+from booking.models import Booking, Profile
 
-# Create your tests here.
+
+class TestUrls(SimpleTestCase):
+    """
+    To test urls.
+    """
+    def test_home_url_is_resolves(self):
+        """ To test the home url """
+        url = reverse('home')
+        response = self.client.get('')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, home)
+        self.assertTemplateUsed(response, 'index.html')
+
+
+    def test_signup_url_is_resolves(self):
+        """ To test the signup url """
+        url = reverse('signup')
+        response = self.client.get('/signup')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, sign_up)
+        self.assertTemplateUsed(response, 'signup.html')
+
+
+    def test_login_url_is_resolves(self):
+        """ To test the login url """
+        url = reverse('login')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, login)
+
+    def test_logout_url_is_resolves(self):
+        """ To test the logout url """
+        url = reverse('logout')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, logout_view)
+
+
+    def test_feedback_url_is_resolves(self):
+        """ To test the feedback url """
+        url = reverse('feedback')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, feedback)
+
+    def test_profile_url_is_resolves(self):
+        """ To test the profile url """
+        url = reverse('profile')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, profile)
+
+    def test_booking_form_url_is_resolves(self):
+        """ To test the booking form url """
+        url = reverse('booking_form')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, booking_form)
+
+    def test_booking_detail_url_is_resolves(self):
+        """ To test the booking detail url """
+        url = reverse('booking_detail', args=['<int:pk>'])
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, booking_detail)
+
+
+
+
+
+
+    
+
+
+
+
